132b6a38dee994726ad7e8b0f346351f7fc3f2f2
Oveerdosee <79644714+Oveerdosee@users.noreply.github.com>
2021-03-04 14:33:15 +0100

    nikton pere

--- Projet 1.py
+++ Projet 1.py
@@ -1,1 +1,30 @@
-﻿#Projet n°1 : le cadavre presque exquis
+﻿#Projet n°1 : histoire
+
+Dans un petit village de Chine, non loin de la ville de Nankin, vivait un jeune homme prénommé Tao. Il était très pauvre, mais cela ne l'empêchait pas d'être généreux et toujours prêt à aider son prochain. Personne ne s'adressait à lui en vain. Un jour, alors que le soleil brillait déjà très haut dans le ciel, Tao, qui dormait sur une paillasse à l'ombre d'un arbre, fut réveillé assez brutalement par un inconnu. Surpris, il ouvrit les yeux et vit devant lui un homme tout de gris vêtu.
+« Réveille-toi, Tao, lui dit l'inconnu. La reine t'attend !
+— La reine ? s'étonna Tao. Mais je ne connais pas de reine !
+— Elle, en revanche, te connaît, poursuivit l'homme en gris, et elle m'a envoyé te chercher de toute urgence. Viens, suis-moi !
+— Mais qui êtes-vous donc ? demanda Tao au messager, je ne vous ai jamais vu ! »
+L'inconnu haussa les épaules : « À quoi cela t'avancerait-il de m'avoir déjà vu et de savoir qui je suis ? La reine a besoin de ton aide. Tu es bien Tao, celui qui ne refuse jamais son aide à personne ? »
+Tao n'osa plus poser de question. Il replia rapidement sa paillasse, et suivit l'inconnu. Ils marchèrent un long moment. À l'instant où Tao crut atteindre les dernières maisons du village, il découvrit devant lui une ville immense dont toutes les habitations, massées les unes contre les autres, présentaient une forme assez étrange, qui lui sembla vaguement familière.
+Le rêve de Tao - illustration 1
+L'inconnu pénétra dans l'une d'elles, plus vaste et somptueuse que les autres. Tao le suivit. Ils arrivèrent dans une salle immense, où une très belle femme était assise sur un trône majestueux. Elle portait dans les cheveux un diadème, qui scintillait de mille feux.
+« Merci d'être venu, murmura-t-elle. Mon royaume court un grand danger et tu es le seul à pouvoir le sauver. » Tao se courba dans un profond salut. « Ce sera un honneur pour moi, Votre Majesté », balbutia-t-il.
+« Je vais te présenter à ma fille, poursuivit la reine d'une voix douce. Je considère tous mes sujets comme mes propres enfants, mais je tiens à ma fille bien plus qu'à moi-même. »
+Tao crut entendre des milliers de clochettes d'or, et une jeune fille, également très belle, entra dans la pièce. Son visage était pâle comme le lys, et ses cheveux de jais coulaient en cascade le long de son dos. L'air infiniment triste, elle alla s'asseoir à côté de la reine, sur une chaise en or.
+À peine se fut-elle installée qu'une dame de la cour entra, tout essoufflée, en hurlant : « Le monstre, le monstre ! » La reine se leva. « Voilà le malheur dont je viens de te parler. Je t'en supplie, Tao, aide ma fille. Elle a pour mission de reconstruire une capitale, mais sans toi, jamais elle n'y parviendra. »
+Tao, sans hésiter une seconde, prit la jeune fille par la main et, ensemble, ils quittèrent le palais discrètement. Pendant des heures, ils coururent sans prendre le temps de retrouver leur souffle. Ils empruntèrent mille et une petites rues tortueuses, et parvinrent finalement au village de Tao. Là, ils purent souffler un peu.
+« Comme c'est calme, ici, soupira Fleur-de-Lotus, car c'est ainsi que la jeune princesse s'appelait.
+— Nous sommes loin de tout danger, à présent, dit Tao.
+— Où allons-nous bâtir la nouvelle capitale ? demanda la princesse.
+— Une capitale ? » demanda Tao, qui n'avait pas très bien compris lorsque la reine lui avait parlé dans son palais. « Mais je ne pourrai jamais construire une capitale. C'est impossible ! Je ne suis qu'un pauvre paysan. Je n'ai ni pouvoir ni argent. »
+La princesse le regarda, et de grosses larmes roulèrent sur ses joues. « Mais tu es pourtant bien Tao, celui qui est toujours prêt à aider son prochain, gémit-elle. Toi seul es capable de le faire…
+— Non, je… » s'apprêtait-il à dire lorsqu'il s'éveilla.
+Le rêve de Tao - illustration 2
+Il avait dû dormir longtemps, car le soleil se trouvait maintenant fort bas sur l'horizon. Bien qu'éveillé, Tao entendait encore la voix suppliante de Fleur-de-Lotus, qui semblait s'éloigner.
+En vérité, il s'agissait d'un essaim d'abeilles.
+Elles semblaient perdues et tournaient dans tous les sens autour des fleurs du jardin. « Pauvres bêtes, pensa Tao, elles n'ont pas de ruche ! Je vais leur en faire fabriquer une. » Et il se rendit immédiatement chez un charpentier. « Je me demande d'où peuvent bien venir toutes ces abeilles ? » pensa-t-il, lorsqu'il vit que les insectes acceptaient avec empressement leur nouveau refuge. Il partit se promener dans le village. Arrivé à hauteur de la dernière maison, il découvrit dans un jardin une ruche abandonnée.
+« J'ai trouvé des abeilles chez moi, dit-il à l'homme qui vivait là. Ne sont-elles pas à vous ?
+— C'est possible, répondit l'homme. Elles ont dû fuir, ajouta-t-il en ôtant le couvercle de la ruche. Comme il se penchait, il y découvrit un serpent. « Oh, le monstre de mon rêve ! », se dit Tao.
+De retour chez lui, il installa dans son jardin toute une rangée de belles ruches semblables. De tous les côtés, des abeilles arrivèrent. Elles se mirent à butiner ses fleurs et lui offrirent tellement de miel en échange de sa protection que Tao, le généreux, devint bientôt riche.
+
